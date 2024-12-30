27ad3a6a10ef6d3b8ac13d53320a2a9046b8b482
Eric Thomas <eathom91@gmail.com>
2024-12-29 19:55:51 -0500

    added scene navigation
    added navigation from scene to scene

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,32 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
